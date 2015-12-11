0e1af7d47f1d4eb982417fb2f8620f9b7845a895
apizzimenti <apizzimenti@uiowa.edu>
2015-12-11 13:22:46 -0600

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+/**
+ * Created by apizzimenti on 12/9/15.
+ */
+
+var date = new Date(),
+    fs = require('fs'),
+    path = require('path'),
+    clc = require('cli-color'),
+    col = clc.xterm(21),
+    rel = path.dirname(require.main.filename),
+    json = JSON.parse(fs.readFileSync(rel + '/compliments.json', 'utf8'));
+
+if (date.getMonth() === 0 && date.getDay() === 29) {
+    console.log(col(json.seasonal[4]));
+    process.exit();
+} else if (date.getMonth() === 6 && date.getDay() === 12) {
+    console.log(col(json.seasonal[1]));
+    process.exit();
+} else if (date.getMonth() === 11 && date.getDay() === 24) {
+    console.log(col(json.seasonal[0]));
+    process.exit();
+} else if (date.getMonth() === 4 && date.getDay() === 25) {
+    console.log(col(json.seasonal[2]));
+    console.log(col(json.seasonal[3]));
+    process.exit();
+} else {
+    var rC = Math.floor(Math.random() * 2);
+
+    if (rC === 0) {
+        console.log(col(json.happy[Math.floor(Math.random() * json.happy.length)]));
+    } else {
+        console.log(col(json.compliments[Math.floor(Math.random() * json.compliments.length)]));
+    }
+}
